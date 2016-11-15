d99ed51a61c6e998a321440c8536023d8f10b6c3
Allan Martín <mayic765@gmail.com>
2016-11-15 00:10:13 -0600

    Add files via upload

---
+++ FrmOperacionesUsuarios.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace SoftEvolution.PRESENTACION
+{
+    partial class FrmOperacionesUsuarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.buscarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.modificarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eliminarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cargarDatosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.regresarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.buscarToolStripMenuItem,
+            this.modificarToolStripMenuItem,
+            this.eliminarToolStripMenuItem,
+            this.cargarDatosToolStripMenuItem,
+            this.regresarToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(589, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // buscarToolStripMenuItem
+            // 
+            this.buscarToolStripMenuItem.Name = "buscarToolStripMenuItem";
+            this.buscarToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.buscarToolStripMenuItem.Text = "Buscar";
+            this.buscarToolStripMenuItem.Click += new System.EventHandler(this.buscarToolStripMenuItem_Click);
+            // 
+            // modificarToolStripMenuItem
+            // 
+            this.modificarToolStripMenuItem.Name = "modificarToolStripMenuItem";
+            this.modificarToolStripMenuItem.Size = new System.Drawing.Size(70, 20);
+            this.modificarToolStripMenuItem.Text = "Modificar";
+            this.modificarToolStripMenuItem.Click += new System.EventHandler(this.modificarToolStripMenuItem_Click_1);
+            // 
+            // eliminarToolStripMenuItem
+            // 
+            this.eliminarToolStripMenuItem.Name = "eliminarToolStripMenuItem";
+            this.eliminarToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.eliminarToolStripMenuItem.Text = "Eliminar";
+            this.eliminarToolStripMenuItem.Click += new System.EventHandler(this.eliminarToolStripMenuItem_Click_1);
+            // 
+            // cargarDatosToolStripMenuItem
+            // 
+            this.cargarDatosToolStripMenuItem.Name = "cargarDatosToolStripMenuItem";
+            this.cargarDatosToolStripMenuItem.Size = new System.Drawing.Size(87, 20);
+            this.cargarDatosToolStripMenuItem.Text = "Cargar Datos";
+            this.cargarDatosToolStripMenuItem.Click += new System.EventHandler(this.cargarDatosToolStripMenuItem_Click_1);
+            // 
+            // regresarToolStripMenuItem
+            // 
+            this.regresarToolStripMenuItem.Name = "regresarToolStripMenuItem";
+            this.regresarToolStripMenuItem.Size = new System.Drawing.Size(64, 20);
+            this.regresarToolStripMenuItem.Text = "Regresar";
+            this.regresarToolStripMenuItem.Click += new System.EventHandler(this.regresarToolStripMenuItem_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(104, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(91, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Usuario a Buscar:";
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(205, 46);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(157, 20);
+            this.txtClave.TabIndex = 8;
+            this.txtClave.TextChanged += new System.EventHandler(this.txtClave_TextChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(14, 99);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(563, 150);
+            this.dataGridView1.TabIndex = 10;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // FrmOperacionesUsuarios
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(589, 261);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtClave);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "FrmOperacionesUsuarios";
+            this.Text = "OperacionesUsuarios";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem buscarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem modificarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem eliminarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cargarDatosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem regresarToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ FrmOperacionesUsuarios.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SoftEvolution.PRESENTACION
+{
+    public partial class FrmOperacionesUsuarios : Form
+    {
+        //Variable para eliminar usuario
+        string campo_Eliminar;
+        //Objeto para comunicarme con otra clase
+        FrmUsuarios obj = new FrmUsuarios();
+        //Creo mi tabla
+        DataGridViewRow dgv;
+        //Constructor de mi clase Buscar
+        public FrmOperacionesUsuarios()
+        {
+            InitializeComponent();
+            InitializeComponent();
+            //Creo un objeto de mi clase en la que estan os diferentes metodos de Cargar,Buscar,Modificar,Eliminar
+            UsuariosDAL datos = new UsuariosDAL();
+            //Cargo todos los datos en mi tabla una ves que se abre está ventana.
+            dataGridView1.DataSource = datos.CargarDatos();
+        }
+
+        private void modificarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void eliminarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cargarDatosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void regresarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        //Revisamos si el txtClave tiene texto, si hay texto mandamos llamar al metodo buscar de la clase UsuariosDAL y ponemos el resultado en 
+        // el dataGridView1, si el campo esta vacio muestra un mensaje de no hay Usuario a buscar
+        private void buscarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (txtClave.Text.Length > 0)
+            {
+                UsuariosDAL buscar = new UsuariosDAL();
+                dataGridView1.DataSource = buscar.BuscarUsuario(txtClave.Text.Trim());
+            }
+            else
+            {
+                MessageBox.Show("No hay Ususario a buscar");
+            }
+        }
+        // Una ves que se selecciona el renglon se cargan los datos en la ventana 1, menos la clave, para guardar los cambios hay que desplegar moodificar y dar
+        // en guardar o cancelar los cambios.
+        private void modificarToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                obj.comboTipo.Text = dgv.Cells[0].Value.ToString();
+                obj.txtClave.Text = dgv.Cells[1].Value.ToString();
+                obj.txtClave.Enabled = false;
+                obj.txtContrasena.Text = dgv.Cells[2].Value.ToString();
+                obj.txtNombre.Text = dgv.Cells[3].Value.ToString();
+                obj.txtApellidos.Text = dgv.Cells[4].Value.ToString();
+                this.Hide();
+                obj.Show();
+                obj.yaseleccionoparaeliminar = 1;
+            }
+            catch
+            {
+                MessageBox.Show("No has seleccionado un usuario a Modificar");
+            }
+        }
+        //Una ves que se selecciona un renglon se pone el valor de la columna 1(Usuario/Clave) el la variable campo_Eliminar y se imprime un mensaje
+        // del campo seleccionado o usuario seleccionado.
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+            dgv = dataGridView1.Rows[e.RowIndex];
+            campo_Eliminar = dgv.Cells[1].Value.ToString();
+
+            MessageBox.Show("Usuario Seleccionado: " + campo_Eliminar);
+        }
+        //recibe un campo usuario_Eliminar de la tabla si la variable es mayor a cero o ya se a seleccionado algun campo usuario entrara y se
+        // pedira la confirmación para eliminar ese usuario si acepta procede a eliminarce y dar a conocer su eliminación y actualización de la 
+        // tabla de lo contrario muestra el mensaje no se ah seleccionado usuario a eliminar.
+        private void eliminarToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            try
+            {
+                if (campo_Eliminar.Length > 0)
+                {
+                    if (MessageBox.Show("Estas seguro de eliminar este registro ?", "Eliminar registro", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        UsuariosDAL eliminar = new UsuariosDAL();
+                        eliminar.EliminarUsuario(campo_Eliminar);
+                        MessageBox.Show("Registro eliminado");
+                        eliminar.CargarDatos();
+                    }
+
+                }
+
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("No has seleccionado el usuario a eliminar.");
+            }
+        }
+        //Metodo que sirve para cargar todos los datos una ves que la caja de texto esta vacia, si hay texto no hace nada.
+        private void txtClave_TextChanged(object sender, EventArgs e)
+        {
+            if (txtClave.Text.Length == 0)
+            {
+                UsuariosDAL datos = new UsuariosDAL();
+                dataGridView1.DataSource = datos.CargarDatos();
+            }
+        }
+        //Metodo del boton CargrarDatos Usa un metodo de la Clase usuariosDAL que carga todos los datos en el DataGridView1. 
+        private void cargarDatosToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            UsuariosDAL datos = new UsuariosDAL();
+            dataGridView1.DataSource = datos.CargarDatos();
+        }
+
+        private void regresarToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            FrmUsuarios a = new FrmUsuarios();
+            a.Show();
+            this.Hide();
+        }
+    }
+}
